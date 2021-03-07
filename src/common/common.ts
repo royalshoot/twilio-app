caa623285d34385b5b4b3be5144f0a34f6a5d9ab
Manuel Linares <royalshootm@gmail.com>
2021-03-08 04:05:28 +0500

    set baseURL in common.ts

--- src/common/common.ts
+++ src/common/common.ts
@@ -2,7 +2,7 @@ import { HttpHeaders } from "@angular/common/http";
 import { LocalStorage } from "./global";
 
 export class AppCommon{
-    static baseUrl: string  = "https://twilio-app.vercel.app/";
+    static baseUrl: string  = "http://www.twilio.somee.com/";
     static getAccountId(){
         return new LocalStorage().get("accountId");
     }
