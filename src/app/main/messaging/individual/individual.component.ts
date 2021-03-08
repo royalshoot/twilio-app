80381804d3ed8042f3864171db461df99731d250
Manuel Linares <royalshootm@gmail.com>
2021-03-09 01:50:36 +0500

    use enter key and scroll to bottom

--- src/app/main/messaging/individual/individual.component.ts
+++ src/app/main/messaging/individual/individual.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AppCommon } from 'src/common/common';
 import { LocalStorage } from 'src/common/global';
 import { ChatService } from 'src/services/chat.service';
@@ -9,14 +9,16 @@ import { ChatService } from 'src/services/chat.service';
   styleUrls: ['./individual.component.css']
 })
 export class IndividualComponent implements OnInit {
-  /**____________DECORATORS____________ */
+/**____________DECORATORS____________ */
+  //@ViewChild('scroller') private messasgeScroller: ElementRef
 
   /**____________PARAMETERS____________ */
   clients: any[] = [];
   messages: any[] = [];
   textMessage: string = "";
-  selectedClient!:number;
+  selectedClient!: number;
   blocked: boolean = false;
+  interval!: any;
 
   /**____________CONSTRUCTORS__________ */
   constructor(private chatService: ChatService) { }
@@ -25,11 +27,11 @@ export class IndividualComponent implements OnInit {
     this.getClients();
   }
   /**____________SERVICE FUNCTIONS_____ */
-  getClients(){
+  getClients() {
     this.blocked = true;
-    this.chatService.getClients().subscribe(res=>{
+    this.chatService.getClients().subscribe(res => {
       this.blocked = false;
-      if(res.success){
+      if (res.success) {
         res.result.forEach(element => {
           element["active"] = false;
         });
@@ -40,36 +42,62 @@ export class IndividualComponent implements OnInit {
   }
 
   /**____________BASIC FUNCTIONS_______ */
-  tapClient(client: any){
+  tapClient(client: any) {
+    if (this.interval) {
+      clearInterval(this.interval);
+      this.interval = undefined;
+    }
     this.selectedClient = client.clientId;
     this.clients.forEach(element => {
       element.active = false;
     });
-    this.clients.filter(m=>m.clientId == client.clientId)[0].active = true;
+    this.clients.filter(m => m.clientId == client.clientId)[0].active = true;
     this.blocked = true;
-    this.chatService.getMessages(client.clientId).subscribe(res=>{
+    this.chatService.getMessages(client.clientId).subscribe(res => {
       this.blocked = false;
-      if(res.success){
+      if (res.success) {
         this.messages = res.result;
+        this.scrollToBottom();
+        this.getContinousMessages(this.selectedClient);
       }
-    })
+    });
   }
-  sendMessage(event:any){
-    if(event){
+
+  scrollToBottom(): void {
+    try {
+      let element = document.getElementsByClassName('msg_history')[0];
+      element.scrollTop = element.scrollHeight;
+      //this.messasgeScroller.nativeElement.scrollTop = this.messasgeScroller.nativeElement.scrollHeight;
+    } catch (err) { }
+  }
+
+  getContinousMessages(clientId: number) {
+    this.interval = setInterval(() => {
+      this.chatService.getMessages(clientId).subscribe(res => {
+        this.blocked = false;
+        if (res.success) {
+          this.messages = res.result;
+        }
+      });
+    }, 2000);
+  }
+
+  sendMessage(event: any) {
+    if (event) {
       let obj = {
         clientId: this.selectedClient,
         body: event.value
       };
       event.value = "";
       this.blocked = true;
-      this.chatService.sendMessage(obj).subscribe(res=>{
+      this.chatService.sendMessage(obj).subscribe(res => {
         this.blocked = false;
-        if(res.success){
+        if (res.success) {
           this.messages.push(res.result);
         }
       });
     }
-    else{
+    else {
       alert('Please select any client to send message');
     }
   }
