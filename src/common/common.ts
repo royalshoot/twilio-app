fd81e450ed870fac1ac7cafb98ec4a16f02d1105
Manuel Linares <royalshootm@gmail.com>
2021-04-23 10:22:52 +0500

    update config for server

--- src/common/common.ts
+++ src/common/common.ts
@@ -2,8 +2,8 @@ import { HttpHeaders } from "@angular/common/http";
 import { LocalStorage } from "./global";
 
 export class AppCommon{
-    //static baseUrl: string = "https://twilio-demo-backend.azurewebsites.net/";
-    static baseUrl: string = "http://localhost:5000/";
+    static baseUrl: string = "https://twilio-demo-backend.azurewebsites.net/";
+    //static baseUrl: string = "http://localhost:5000/";
     static getAccountId(){
         return new LocalStorage().get("accountId");
     }
